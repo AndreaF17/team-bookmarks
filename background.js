5764d9ec2bd6a1450e95df2949e5eb1138a23411
Andrea Ferrario <andre.ferrario@icloud.com>
2024-08-06 17:07:07 +0200

    project init

---
+++ background.js
@@ -0,0 +1,31 @@
+// Define findOrCreateCveFolder as an async function
+async function findOrCreateFolder(folderName) {
+  console.debug("looking for folder: ", folderName);
+  let folderId;
+  const folders = await chrome.bookmarks.search({ title: folderName });
+  if (folders.length > 0) {
+    console.debug(folderName, " folder already exists");
+    folderId = folders[0].id;
+  } else {
+    console.debug(folderName, " folder does not exist, creating...");
+    const newFolder = await chrome.bookmarks.create({ title: folderName });
+    folderId = newFolder.id;
+  }
+  return folderId;
+}
+
+function addBookmark(folderId, bookmarkTitle, bookmarkUrl) {
+  if (bookmarkTitle && bookmarkUrl && folderId) {
+    chrome.bookmarks.create({
+      parentId: folderId,
+      title: bookmarkTitle,
+      url: bookmarkUrl,
+    });
+    console.debug("Bookmark added: " + bookmarkTitle + " - " + bookmarkUrl);
+  }
+}
+
+(async () => {
+  const exampleFolderid = await findOrCreateFolder();
+  console.log("exampleFolderid: ", exampleFolderid);
+})();
